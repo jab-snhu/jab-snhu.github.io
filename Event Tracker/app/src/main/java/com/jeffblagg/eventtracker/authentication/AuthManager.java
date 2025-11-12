9bd64f5671595c81f13af9bf8a12021a01412475
Jeff Blagg <jeffblagg@gmail.com>
2025-11-11 21:04:19 -0800

    Adds Firebase authentication manager to Event Tracker

---
+++ Event Tracker/app/src/main/java/com/jeffblagg/eventtracker/authentication/AuthManager.java
@@ -0,0 +1,73 @@
+package com.jeffblagg.eventtracker.authentication;
+
+/**
+ * Interface for handling authentication processes. Provides methods for
+ * creating accounts, signing in and out, and getting the id of the currently
+ * logged in user.
+ */
+public interface AuthManager {
+
+    /**
+     * Represents the possible authentication errors relevant to the sign in
+     * or account creation process.
+     * <p>
+     *     Possible values:
+     * <ul>
+     *     <li>{@link #USER_NOT_FOUND} - No account exists for the email provided</li>
+     *     <li>{@link #INVALID_PASSWORD} - Invalid password provided</li>
+     *     <li>{@link #OTHER} - Any other authentication error</li>
+     * </ul>
+     * </p>
+     */
+    public enum AuthError {
+        USER_NOT_FOUND("No account exists with this email"),
+        INVALID_PASSWORD("Invalid password"),
+        OTHER("Authentication error occurred");
+
+        public final String message;
+
+        AuthError(String message) {
+            this.message = message;
+        }
+    }
+
+    /**
+     * Interface for authentication callbacks
+     */
+    public interface AuthCallback {
+        void onSuccess(String userId);
+        void onFailure(AuthError error);
+    }
+
+    /**
+     * Get the user ID for the currently logged in user.
+     *
+     * @return user ID or null if no user is logged in
+     */
+    String getCurrentUserId();
+
+    /**
+     * Attempts to sign a user in with the provided email and password.
+     *
+     * @param email the email address for the user
+     * @param password the password for the user
+     * @param callback the callback used to return the logged in user id if
+     *                 successful or the appropriate {@link AuthError} if unsuccessful
+     */
+    void signIn(String email, String password, AuthCallback callback);
+
+    /**
+     * Creates a new account with the provided email and password.
+     *
+     * @param email the email for the new account
+     * @param password the password for the new account
+     * @param callback the callback used to return the user id of the created
+     *                 account or the appropriate {@link AuthError} if unsuccessful
+     */
+    void createAccount(String email, String password, AuthCallback callback);
+
+    /**
+     * Signs the currently logged in user out.
+     */
+    void signOut();
+}
---
+++ Event Tracker/app/src/main/java/com/jeffblagg/eventtracker/authentication/FirebaseAuthManager.java
@@ -0,0 +1,83 @@
+package com.jeffblagg.eventtracker.authentication;
+
+import com.google.firebase.auth.*;
+
+/**
+ * A manager class to handle all authentication actions with Firebase
+ * Authentication. Implements the AuthManager interface.
+ */
+public class FirebaseAuthManager implements AuthManager {
+    private final FirebaseAuth firebaseAuth;
+
+    public FirebaseAuthManager() {
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public String getCurrentUserId() {
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        if (user != null) {
+            return user.getUid();
+        }
+
+        return null;
+    }
+
+    @Override
+    public void signIn(String email, String password, AuthCallback callback) {
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                        if (user != null) {
+                            callback.onSuccess(user.getUid());
+                        } else {
+                            callback.onFailure(AuthError.OTHER);
+                        }
+                    } else {
+                        callback.onFailure(authErrorFromException(task.getException()));
+                    }
+                });
+    }
+
+    @Override
+    public void createAccount(String email, String password, AuthCallback callback) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                        if (user != null) {
+                            callback.onSuccess(user.getUid());
+                        } else {
+                            callback.onFailure(AuthError.OTHER);
+                        }
+                    } else {
+                        callback.onFailure(authErrorFromException(task.getException()));
+                    }
+                });
+    }
+
+    @Override
+    public void signOut() {
+        firebaseAuth.signOut();
+    }
+
+    /**
+     * Provides an appropriate AuthError for a provided Firebase exception.
+     *
+     * @param exception the Firebase exception
+     * @return the mapped AuthError
+     */
+    private AuthError authErrorFromException(Exception exception) {
+        if (exception instanceof FirebaseAuthInvalidUserException) {
+            return AuthError.USER_NOT_FOUND;
+        } else if (exception instanceof FirebaseAuthInvalidCredentialsException) {
+            return AuthError.INVALID_PASSWORD;
+        } else {
+            return AuthError.OTHER;
+        }
+    }
+}
